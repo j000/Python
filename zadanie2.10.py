a43ec67b23b359680955d7163f74c7130df47275
j000 <jarymut+github@gmail.com>
2019-08-22 17:54:09 +0200

    Zadanie 2.10 v4

--- zadanie2.10.py
+++ zadanie2.10.py
@@ -4,12 +4,16 @@ import sys
 def count_words(line):
     return len(line.split())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # testy
     assert 1 == count_words('test')
     assert 2 == count_words('test test')
-    assert 2 == count_words('test  	test') # sp sp tab
-    assert 2 == count_words('test 	 test') # sp tab sp
+    assert 2 == count_words('test  \ttest')
+    assert 2 == count_words('test \t test')
+    assert 2 == count_words('test\t  test')
+    assert 2 == count_words('test  \ntest')
+    assert 2 == count_words('test \n test')
+    assert 2 == count_words('test\n  test')
     # policz wyrazy na standardowym wejściu
     data = ''.join(sys.stdin.readlines())
     print('Wyrazy:', count_words(data))
