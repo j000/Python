c37f859bde264be3d2c8bf062a9f4037a0a3e63a
j000 <jarymut+github@gmail.com>
2019-10-06 14:50:30 +0200

    Adhere to PEP-8

--- TreeClass.py
+++ TreeClass.py
@@ -1,39 +1,39 @@
 class Node:
-	def __init__(self, value = None):
-		self.left = None
-		self.right = None
-		self.data = value
+    def __init__(self, value = None):
+        self.left = None
+        self.right = None
+        self.data = value
 
-	def __print(self):
-		if self.left is not None:
-			self.left.__print()
-		print(self.data, end = ' ')
-		if self.right is not None:
-			self.right.__print()
+    def __print(self):
+        if self.left is not None:
+            self.left.__print()
+        print(self.data, end = ' ')
+        if self.right is not None:
+            self.right.__print()
 
 class Tree:
-	def __init__(self):
-		self.root = None
+    def __init__(self):
+        self.root = None
 
-	def print(self):
-		if self.root is not None:
-			self.root._Node__print()
-		print()
+    def print(self):
+        if self.root is not None:
+            self.root._Node__print()
+        print()
 
-	def insert(self, value):
-		if self.root is None:
-			self.root = Node(value)
-			return
-		tmp = self.root
-		while (1):
-			if value < tmp.data:
-				if tmp.left is None:
-					tmp.left = Node(value)
-					return
-				tmp = tmp.left
-				continue
-			if tmp.right is None:
-				tmp.right = Node(value)
-				return
-			tmp = tmp.right
-			continue
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+            return
+        tmp = self.root
+        while (1):
+            if value < tmp.data:
+                if tmp.left is None:
+                    tmp.left = Node(value)
+                    return
+                tmp = tmp.left
+                continue
+            if tmp.right is None:
+                tmp.right = Node(value)
+                return
+            tmp = tmp.right
+            continue
