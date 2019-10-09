1b0979b998aa5806cc5628f8172db89442984fb3
j000 <jarymut+github@gmail.com>
2019-10-09 14:54:22 +0200

    Zadanie 2.19: fix

--- zadanie2.19.py
+++ zadanie2.19.py
@@ -3,7 +3,17 @@ import sys
 import random
 
 def fun(list):
-    return map(lambda x: str.zfill(str(x), 3), list)
+    return map(
+        lambda x: str.zfill(str(x), 3),
+        list
+    )
 
 if __name__ == '__main__':
-    print(', '.join(fun([random.randint(0,999) for _ in range(random.randint(10,20))])))
+    print(
+        ', '.join(
+            fun(
+                [
+                    random.randint(0,999) for _ in range(random.randint(10,20))
+                ]
+            )
+        ))
