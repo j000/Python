8776da7acf1caf16fe9767049dbcd2f2c72cf2c0
j000 <jarymut+github@gmail.com>
2019-10-10 14:41:17 +0200

    Zadanie 4.7: fix

--- zestaw04/zadanie07.py
+++ zestaw04/zadanie07.py
@@ -9,6 +9,19 @@ def flatten(sequence):
             out.append(i)
     return out
 
+def better_flatten(sequence):
+    out = []
+    stack = [sequence]
+    while len(stack):
+        tmp = stack.pop()
+        if isinstance(tmp, (list, tuple)):
+            stack.extend(tmp)
+        else:
+            out.append(tmp)
+    out.reverse()
+    return out
+
 seq = [1,(2,3),[],[4,(5,6,7)],8,[9]]
 # [1,2,3,4,5,6,7,8,9]
 print(flatten(seq))
+print(better_flatten(seq))
