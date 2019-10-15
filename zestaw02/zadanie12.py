f73bbb365cd542e422ee630fd1bcb083acdd61d1
j000 <jarymut+github@gmail.com>
2019-10-15 23:21:19 +0200

    Add tests to 2.12 and 2.13

--- zestaw02/zadanie12.py
+++ zestaw02/zadanie12.py
@@ -1,28 +1,50 @@
 #!/usr/bin/env python3
+"""
+zadanie 2.12
+"""
 import sys
 
 def first_chars(line):
+    """
+    Returns string made of first characters of every word in line.
+
+    >>> first_chars('Nobody is calculating expressions.')
+    'Nice'
+    >>> first_chars('Lorem ipsum dolor sit amet')
+    'Lidsa'
+    """
     out = ''
     for i in line.split():
         out += list(i)[0]
     return out
 
 def last_chars(line):
+    """
+    Returns string made of last characters of every word in line.
+
+    >>> last_chars('Lorem ipsum dolor sit amet')
+    'mmrtt'
+    """
     out = ''
     for i in line.split():
         out += list(i)[-1]
     return out
 
 if __name__ == '__main__':
+    # testy
+    import doctest
+    doctest.testmod()
+
     if len(sys.argv) > 1:
         # w podanych plikach
         for filename in sys.argv[1:]:
+            if filename.startswith('-'):
+                continue
             with open(filename) as f:
                 print(f'{filename}:', first_chars(f.read()))
                 print(f'{filename}:', last_chars(f.read()))
-                f.close()
     else:
         # na standardowym wejściu
-        data = sys.stdin.read()
+        data = sys.stdin.read().rstrip()
         print(first_chars(data))
         print(last_chars(data))
--- zestaw02/zadanie13.py
+++ zestaw02/zadanie13.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
+"""
+zadanie 2.13
+"""
 import sys
 
 def count_letters(line):
+    """
+    Returns sum of lengths of all words in line.
+
+    >>> count_letters('first test')
+    9
+    >>> count_letters('Lorem ipsum dolor sit amet')
+    22
+    """
     return sum(map(len, line.split()))
 
 if __name__ == '__main__':
     # testy
-    assert 4 == count_letters('test')
-    # policz litery
+    import doctest
+    doctest.testmod()
+
     if len(sys.argv) > 1:
         # w podanych plikach
         for filename in sys.argv[1:]:
+            if filename.startswith('-'):
+                continue
             with open(filename) as f:
-                print(f'{filename}: Liter:', count_letters(f.read()))
-                f.close()
+                print(f'{filename}: Liter: {count_letters(f.read())}')
     else:
         # na standardowym wejściu
-        data = sys.stdin.read()
-        print('Liter:', count_letters(data))
+        data = sys.stdin.read().rstrip()
+        print(f'Liter: {count_letters(data)}')
