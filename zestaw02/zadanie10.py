c18076749f8898223fefc3e81c92c70e186f7927
j000 <jarymut+github@gmail.com>
2019-10-15 23:08:33 +0200

    Start using docstrings and doctest

--- zestaw02/zadanie10.py
+++ zestaw02/zadanie10.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python3
+"""
+zadanie 2.10
+"""
 import sys
 
 def count_words(line):
+    """Funkcja liczy wyrazy w podanym stringu
+
+    >>> count_words('test')
+    1
+    >>> count_words('test test')
+    2
+    >>> count_words('test \
+    test')
+    2
+    >>> count_words('one, two, three words here')
+    5
+    """
     return len(line.split())
 
 if __name__ == '__main__':
     # testy
-    assert 1 == count_words('test')
-    assert 2 == count_words('test test')
-    assert 2 == count_words('test  \ttest')
-    assert 2 == count_words('test \t test')
-    assert 2 == count_words('test\t  test')
-    assert 2 == count_words('test  \ntest')
-    assert 2 == count_words('test \n test')
-    assert 2 == count_words('test\n  test')
-    # policz wyrazy
+    import doctest
+    doctest.testmod()
+
     if len(sys.argv) > 1:
         # w podanych plikach
         for filename in sys.argv[1:]:
+            if filename.startswith('-'):
+                continue
             with open(filename) as f:
-                print(f'{filename}: Wyrazy:', count_words(f.read()))
-                f.close()
+                print(f'{filename}: Wyrazy: {count_words(f.read())}')
     else:
         # na standardowym wejściu
-        data = sys.stdin.read()
+        data = sys.stdin.read().rstrip()
         print('Wyrazy:', count_words(data))
