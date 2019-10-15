f4725bdeae77d3f00b1e0d692a50cd8da2c22b9f
j000 <jarymut+github@gmail.com>
2019-10-15 23:09:12 +0200

    Add tests to 2.11

--- zestaw02/zadanie11.py
+++ zestaw02/zadanie11.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
+"""
+zadanie 2.11
+"""
 import sys
 
 def improve(line):
+    """Funkcja rodziela znaki wstawiając znak '_'
+
+    >>> improve('ab')
+    'a_b'
+    >>> improve('abc')
+    'a_b_c'
+    >>> improve('__')
+    '___'
+    >>> improve('a')
+    'a'
+    >>> improve('  ')
+    ' _ '
+    """
     return "_".join(list(line))
 
 if __name__ == '__main__':
     # testy
-    assert 't' == improve('t');
-    assert 't_t' == improve('tt');
-    # popraw
+    import doctest
+    doctest.testmod()
+
     if len(sys.argv) > 1:
         # w podanych plikach
         for filename in sys.argv[1:]:
+            if filename.startswith('-'):
+                continue
             with open(filename) as f:
-                print(f'{filename}:', improve(f.read()))
-                f.close()
+                print(f'{filename}: {improve(f.read())}')
     else:
         # na standardowym wejściu
-        data = sys.stdin.read()
+        data = sys.stdin.read().rstrip()
         print(improve(data))
