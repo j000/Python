33456db1997935b60db7322ffc7cd6873c38ac4e
j000 <jarymut+github@gmail.com>
2019-10-16 10:17:24 +0200

    Add tests to 2.15

--- zestaw02/zadanie15.py
+++ zestaw02/zadanie15.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
+"""
+zadanie 2.15
+"""
 import sys
 
-def supernumber(line):
+def supernumber(list):
+    """
+    Returns string made of numbers from list.
+
+    >>> supernumber([42])
+    '42'
+    >>> supernumber([4, 2])
+    '42'
+    >>> supernumber([])
+    ''
+    """
     out = ''
-    for i in line:
+    for i in list:
         out += str(i)
     return out
 
 if __name__ == '__main__':
+    # testy
+    import doctest
+    doctest.testmod()
+
     if len(sys.argv) > 1:
         # w podanych plikach
         for filename in sys.argv[1:]:
+            if filename.startswith('-'):
+                continue
             with open(filename) as f:
-                print(f'{filename}:', supernumber(map(chr, list(f.read()))))
-                f.close()
+                print(f'{filename}: {supernumber(map(chr, list(f.read())))}')
     else:
         # na standardowym wejściu
-        data = map(ord, list(sys.stdin.read()))
+        data = map(ord, list(sys.stdin.read().rstrip()))
         print(supernumber(data))
