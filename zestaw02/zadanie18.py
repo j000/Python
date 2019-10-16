258c5b3e2b1185c858d6326e7f028dacd43af247
j000 <jarymut+github@gmail.com>
2019-10-16 14:20:09 +0200

    Add tests to 2.18

--- zestaw02/zadanie18.py
+++ zestaw02/zadanie18.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
+"""
+zadanie 2.18
+"""
 import sys
 
 def zeros(number):
+    """
+    Returns number of zeros in number.
+
+    >>> zeros(0)
+    1
+    >>> zeros(1)
+    0
+    >>> zeros(10)
+    1
+    >>> zeros(101010101010)
+    6
+    """
     return str(number).count('0')
 
 def print_zeros(number):
+    """
+    Prints number and count of zeros.
+    """
     print(number, ':', zeros(number))
 
 if __name__ == '__main__':
+    # testy
+    import doctest
+    doctest.testmod()
+
     print_zeros(42)
     print_zeros(123456789)
     print_zeros(102030405060708090)
