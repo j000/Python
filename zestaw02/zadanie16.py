db5380782ec6a62adfe174ccb9fbaaa8e59d44fb
j000 <jarymut+github@gmail.com>
2019-10-16 10:23:26 +0200

    Add tests to 2.16

--- zestaw02/zadanie16.py
+++ zestaw02/zadanie16.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
+"""
+zadanie 2.16
+"""
 import sys
 
 def fix(line):
+    """
+    Replaces text 'GvR' to 'Guido van Rossum' in line.
+
+    >>> fix('')
+    ''
+    >>> fix('test')
+    'test'
+    >>> fix('test GvR')
+    'test Guido van Rossum'
+    >>> fix('"testGvR"')
+    '"testGuido van Rossum"'
+    >>> fix('GvR test GvR')
+    'Guido van Rossum test Guido van Rossum'
+    """
     return line.replace('GvR', 'Guido van Rossum')
 
 if __name__ == '__main__':
+    # testy
+    import doctest
+    doctest.testmod()
+
     if len(sys.argv) > 1:
         # w podanych plikach
         for filename in sys.argv[1:]:
+            if filename.startswith('-'):
+                continue
             with open(filename) as f:
-                print(f'{filename}:', fix(f.read()), end='')
-                f.close()
+                print(f'{filename}: {fix(f.read())}')
     else:
         # na standardowym wejściu
-        print(fix(sys.stdin.read()), end='')
+        data = sys.stdin.read().rstrip()
+        print(fix(data))
