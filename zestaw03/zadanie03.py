7863f97c846f2fbac5397c5dc64940224c8aa34f
j000 <jarymut+github@gmail.com>
2019-10-23 22:14:35 +0200

    Doctest works for printing too!

--- zestaw03/zadanie03.py
+++ zestaw03/zadanie03.py
@@ -5,7 +5,15 @@ zadanie 3.3
 """
 
 def wypisz_liczby_niepodzielne_przez_3():
+    """
+    >>> wypisz_liczby_niepodzielne_przez_3()
+    1 2 4 5 7 8 10 11 13 14 16 17 19 20 22 23 25 26 28 29
+    """
     print(*[x for x in range(30) if (x % 3 != 0)])
 
 if __name__ == '__main__':
+    # testy
+    import doctest
+    doctest.testmod()
+
     wypisz_liczby_niepodzielne_przez_3()
