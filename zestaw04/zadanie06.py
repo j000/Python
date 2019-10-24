77995d61cd9653102feb7d43f4c707c3f711731e
j000 <jarymut+github@gmail.com>
2019-10-24 02:00:00 +0200

    Add task 4.6

---
+++ zestaw04/zadanie06.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+zadanie 4.6
+"""
+
+def  sum_seq(sequence):
+    """
+    Returns sum of numbers in (nested) sequence
+    >>> sum_seq([1])
+    1
+    >>> sum_seq([])
+    0
+    >>> sum_seq([1, [2, [3]]])
+    6
+    >>> sum_seq((1, 2))
+    3
+    >>> sum_seq([1,(2,3),[],[4,(5,6,7)],8,[9]])
+    45
+    """
+    out = 0
+    stack = [sequence]
+    while len(stack):
+        tmp = stack.pop()
+        if isinstance(tmp, (list, tuple)):
+            stack.extend(tmp)
+        else:
+            out += tmp
+    return out
+
+if __name__ == '__main__':
+    # testy
+    import doctest
+    doctest.testmod()
+
+    seq = [1,(2,3),[],[4,(5,6,7)],8,[9]]
+    # [1,2,3,4,5,6,7,8,9]
+    print(f'{seq} => {sum_seq(seq)}')
