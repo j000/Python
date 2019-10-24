8b149bcf40409250cb672d3f0f1eccc97245ba39
j000 <jarymut+github@gmail.com>
2019-10-25 01:00:00 +0200

    Fix old bug

--- zestaw03/zadanie08.py
+++ zestaw03/zadanie08.py
@@ -5,6 +5,25 @@ Jarosław Rymut
 zadanie 3.8
 """
 
+def is_hashable(sequence):
+    """
+    Checks if all elements of sequence can be hashed
+
+    >>> is_hashable([1, 2])
+    True
+    >>> is_hashable([(1), (2)])
+    True
+    >>> is_hashable([[1], [2]])
+    False
+    >>> is_hashable([{1: '1'}])
+    False
+    """
+    from collections.abc import Hashable
+    for i in sequence:
+        if not isinstance(i, Hashable):
+            return False
+    return True
+
 def common(seq1, seq2):
     """
     Returns list of elemests that exist in both seq1 and seq2
@@ -19,8 +38,23 @@ def common(seq1, seq2):
     []
     >>> common([], [])
     []
+    >>> common([[1], [2]], [[1]])
+    [[1]]
+    >>> common([(1)], [[1]])
+    []
+    >>> common([0, 1], [1, 0])
+    [0, 1]
     """
-    return list(set(seq1) & set(seq2))
+    if is_hashable(seq1) and is_hashable(seq2):
+        return list(set(seq1) & set(seq2))
+    out = []
+    for i in seq1:
+        for j in seq2:
+            if str(i) == str(j):
+                out.append(i)
+                break
+
+    return out
 
 def all(seq1, seq2):
     """
@@ -36,6 +70,12 @@ def all(seq1, seq2):
     [0]
     >>> all([], [])
     []
+    >>> all([[1]], [[1]])
+    [[1]]
+    >>> all([[1], [2]], [[1]])
+    [[1], [2]]
+    >>> all([(1)], [[1]])
+    [(1), [1]]
     """
     return list(set(seq1 + seq2))
 
