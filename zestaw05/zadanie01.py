ffcdd4b38ed9b60dd1f9a4706a1281c2fe96122a
j000 <jarymut+github@gmail.com>
2019-10-24 04:00:00 +0200

    Add task 5.1

---
+++ zestaw05/zadanie01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+zadanie 5.1
+"""
+
+import sys
+sys.path.insert(0, '../zestaw04')
+
+####################
+
+from zadanie04 import fibonacci
+print(*[fibonacci(x) for x in range(12)],'…')
+
+####################
+
+def silnia(n):
+    return 1
+print(*[silnia(x) for x in range(5)])
+from zadanie03 import silnia
+print(*[silnia(x) for x in range(5)])
