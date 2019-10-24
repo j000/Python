33bcaf49dc770264b61931c323d9df9c39cdf512
j000 <jarymut+github@gmail.com>
2019-10-24 05:00:00 +0200

    Fix task 4.5

--- zestaw04/zadanie05.py
+++ zestaw04/zadanie05.py
@@ -23,6 +23,27 @@ def odwracanie(L, left, right):
     # return L[:left] + L[right:left-1:-1] + L[right+1:]
     return L[:left] + L[left:right+1][::-1] + L[right+1:]
 
+def odwracanie_rec(L, left, right):
+    """
+    Return list L with L[left:right+1] reversed - recursive version
+    >>> odwracanie_rec([1, 2, 3, 4], 1, 2)
+    [1, 3, 2, 4]
+    >>> odwracanie_rec([1, 2, 3, 4], 0, 1)
+    [2, 1, 3, 4]
+    >>> odwracanie_rec([1, 2, 3, 4], 2, 3)
+    [1, 2, 4, 3]
+    >>> odwracanie_rec([1, 2, 3, 4], 0, 3)
+    [4, 3, 2, 1]
+    >>> odwracanie_rec([1, 2], 0, 0)
+    [1, 2]
+    >>> odwracanie_rec([1, 2], 2, 3)
+    [1, 2]
+    """
+    if left >= right or left >= len(L) or right <= 0:
+        return L
+    L[left], L[right] = L[right], L[left]
+    return odwracanie_rec(L, left + 1, right - 1)
+
 if __name__ == '__main__':
     # testy
     import doctest
