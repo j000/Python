f281bfa55449d288b65fa97a700dbabd7f41a888
j000 <jarymut+github@gmail.com>
2019-10-24 21:00:00 +0200

    Avoid recursion

--- zestaw08/dynamic.py
+++ zestaw08/dynamic.py
@@ -7,24 +7,45 @@ Jarosław Rymut
 """
 
 class Cache:
-    a = []
+    a = [[0.5]]
 
     def __getitem__(self, ij):
         i, j = ij
+        if i <= 0 and j <= 0:
+            return 0.5
+        if j <= 0:
+            return 0.0
+        if i <= 0:
+            return 1.0
         try:
             return Cache.a[i][j]
         except IndexError:
-            return None
+            self.__fill(i, j)
+            return Cache.a[i][j]
 
     def __setitem__(self, ij, x):
         i, j = ij
-        if len(Cache.a) <= i:
-            Cache.a = Cache.a + [[]] * (1 + i - len(Cache.a))
-        tmp = Cache.a[i]
-        if len(tmp) <= j:
-            tmp += [None] * (1 + j - len(tmp))
         Cache.a[i][j] = x
         return x
+
+    def print(self):
+        for row in Cache.a:
+            for item in row:
+                print(f' {item:-8.4f}', end='')
+            print()
+    
+    def __fill(self, x, y):
+        if len(Cache.a) > x and len(Cache.a[x]) > y:
+            return
+        Cache.a += [[0.0] for i in range(1 + x - len(Cache.a))]
+        Cache.a[0] += [1.0] * (1 + y - len(Cache.a[0]))
+        for i in range(1, x + 1):
+            old_len = len(Cache.a[i])
+            if old_len <= y:
+                Cache.a[i] += [None] * (1 + y - old_len)
+                for j in range(old_len, y + 1):
+                    Cache.a[i][j] = 0.5 * (Cache.a[i - 1][j] + Cache.a[i][j - 1])
+
 
 def p_rec(i, j):
     if i > 10 or j > 10 or i + j > 15:
@@ -46,22 +67,24 @@ def p(i, j):
         return 0.0
     if i <= 0:
         return 1.0
-    tmp1 = Cache()[i - 1, j]
-    if tmp1 is None:
-        tmp1 = Cache()[i - 1, j] = p(i - 1, j)
-    tmp2 = Cache()[i, j - 1]
-    if tmp2 is None:
-        tmp2 = Cache()[i, j - 1] = p(i, j - 1)
-    return 0.5 * (tmp1 + tmp2)
+    return Cache()[i, j]
 
 if __name__ == '__main__':
     print(p(2, 4))
-    print(p_rec(2, 4))
+    print('rec:', p_rec(2, 4))
+    print(p(6, 1))
+    print('rec:', p_rec(6, 1))
+    print(p(4, 7))
+    print('rec:', p_rec(4, 7))
     print(p(20, 40))
+    print(p(1_000, 1_000))
+    # print(p(10_000, 10_000))
     # print(p_rec(20, 40))
 
     import timeit
 
-    print('timeit p(5, 7):', timeit.timeit('p(5, 7)', setup='from __main__ import p; p(5, 7)', number=10_000))
-    print('timeit p_rec(5, 7):', timeit.timeit('p_rec(5, 7)', setup='from __main__ import p_rec', number=10_000))
-    print('timeit p(200, 400):', timeit.timeit('p(200, 400)', setup='from __main__ import p; p(200, 400)', number=10_000))
+    number = 10_000
+
+    print('timeit p(5, 7):', timeit.timeit('p(5, 7)', setup='from __main__ import p; p(5, 7)', number=number))
+    print('timeit p_rec(5, 7):', timeit.timeit('p_rec(5, 7)', setup='from __main__ import p_rec', number=number))
+    print('timeit p(200, 400):', timeit.timeit('p(200, 400)', setup='from __main__ import p; p(200, 400)', number=number))
